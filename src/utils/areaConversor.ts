505fd5132d15662d3f24fcaae4464d64926c1860
yurecouto <yure.couto@hotmail.com>
2023-06-06 16:09:37 -0300

    working on cylinder screen

--- src/utils/areaConversor.ts
+++ src/utils/areaConversor.ts
@@ -5,6 +5,8 @@ export function areaConversor(valor: number, unidadeAtual: string, unidadeDestin
     "m2": 1000000,
     "km2": 1000000000000,
     "in2": 645.16,
+
+
     "ft2": 0.0000107639,
     "yd2": 0.000119599,
     "mi2": 0.000000000386102,
